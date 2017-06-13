6ba289dd186827f06e32c1a55893b5d821d51304
Ania Polak <tunczykzcytryna@gmail.com>
2017-06-13 10:19:24 +0200

    Add class structure

--- src/game/Board.java
+++ src/game/Board.java
@@ -1,4 +1,20 @@
 package game;
 
 public class Board {
+    private Cell cells;
+
+    public Board() {
+    }
+
+    public void init() {
+    }
+
+    public boolean isDraw() {
+        return true;
+    }
+
+    public boolean hasWon(Seed seed, int row, int col){
+        return false;
+    }
+
 }
