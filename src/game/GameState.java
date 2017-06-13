132dad53ce896fa59d6f250e1480c7da2764f435
Ania Polak <tunczykzcytryna@gmail.com>
2017-06-13 09:50:18 +0200

    Add enums

--- src/game/GameState.java
+++ src/game/GameState.java
@@ -1,4 +1,11 @@
 package game;
 
 public enum GameState {
+    PLAYING,
+    DRAW,
+    CROSS_WON,
+    NOUGHT_WON;
+
+    GameState(){
+    }
 }
--- src/game/Seed.java
+++ src/game/Seed.java
@@ -1,4 +1,17 @@
 package game;
 
 public enum Seed {
+    EMPTY (" "),
+    CROSS ("X"),
+    NOUGHT ("O");
+
+    private final String symbol;
+
+    Seed(String symbol){
+        this.symbol = symbol;
+    }
+
+    private String symbol(){
+        return symbol;
+    }
 }
