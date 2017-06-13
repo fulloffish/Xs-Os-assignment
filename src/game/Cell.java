6a48c3765eb461cc5b05f99dda8dd32a59c9c3c0
Ania Polak <tunczykzcytryna@gmail.com>
2017-06-13 10:03:31 +0200

    Add class structure

--- src/game/Cell.java
+++ src/game/Cell.java
@@ -1,4 +1,31 @@
 package game;
 
 public class Cell {
+    private int row;
+    private int col;
+
+
+    public Cell(int row, int col){
+      this.setRow(row);
+      this.setCol(col);
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
+
+    public void clear() {
+    }
 }
