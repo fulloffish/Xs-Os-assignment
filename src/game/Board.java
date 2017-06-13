e91212021cf435023f39a663c5e2c30d23989d8f
ika.grabon <ika.grabon@gmail.com>
2017-06-13 14:28:06 +0200

    Add options in hasWon

--- src/game/Board.java
+++ src/game/Board.java
@@ -25,8 +25,32 @@ public class Board {
         cells[0][2].setContent(Seed.CROSS);
         if (cells[0][0].getContent().equals(cells[0][1].getContent())
                 && cells[0][1].getContent().equals(cells[0][2].getContent())) {
-            System.out.println("true");
+            System.out.println("ROW1");
             return true;
+        }
+        if (cells[1][0].getContent().equals(cells[1][1].getContent())
+            && cells[1][1].getContent().equals(cells[1][2].getContent())) {
+            System.out.println("ROW2");
+            return true;
+        }
+        if (cells[2][0].getContent().equals(cells[2][1].getContent())
+            && cells[2][1].getContent().equals(cells[2][2].getContent())) {
+            System.out.println("ROW3");
+            return true;
+        }
+        if (cells[0][0].getContent().equals(cells[1][0].getContent())
+            && cells[1][0].getContent().equals(cells[2][0].getContent())) {
+            System.out.println("COL1");
+            return true;
+        }
+        if (cells[0][1].getContent().equals(cells[1][1].getContent())
+            && cells[1][1].getContent().equals(cells[2][1].getContent())) {
+            System.out.println("COL2");
+            return true;
+        }
+        if (cells[0][2].getContent().equals(cells[1][2].getContent())
+            && cells[1][2].getContent().equals(cells[2][2].getContent())) {
+            System.out.println("COL3");
         }
         return false;
     }
