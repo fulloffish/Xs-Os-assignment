6ab42642269b79813a45725786071cfe1dd130f5
Ania Polak <tunczykzcytryna@gmail.com>
2017-06-13 11:48:12 +0200

    Add init method

--- src/game/Board.java
+++ src/game/Board.java
@@ -1,12 +1,18 @@
 package game;
 
 public class Board {
-    private Cell cells;
+    private Cell[][] cells;
 
     public Board() {
+        cells = new Cell[3][3];
     }
 
     public void init() {
+        for(int rowIndex =0; rowIndex <this.cells.length; rowIndex++){
+            for(int columnIndex =0; columnIndex< this.cells[rowIndex].length; columnIndex++) {
+                this.cells[rowIndex][columnIndex] = new Cell(rowIndex, columnIndex);
+            }
+        }
     }
 
     public boolean isDraw() {
@@ -16,5 +22,6 @@ public class Board {
     public boolean hasWon(Seed seed, int row, int col){
         return false;
     }
+
 
 }
