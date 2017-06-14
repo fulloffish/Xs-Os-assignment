4fdf7a65342645f6fc6996d74a7ff583f0ae648b
Krzychu <sk@in.pl>
2017-06-14 14:06:52 +0200

    cd

--- src/Application.java
+++ src/Application.java
@@ -12,7 +12,6 @@ public class Application {
         gameController.startGame();
 
         do {
-
             gameController.play();
             playAgain = gameController.wantToPlayAgain();
         }
