c50f4c4f912c2a14b94599d06556fce334327683
ika.grabon <ika.grabon@gmail.com>
2017-06-14 11:27:25 +0200

    Add showBoard

--- src/io/UserOutput.java
+++ src/io/UserOutput.java
@@ -1,11 +1,13 @@
 package io;
 
+import game.Board;
 import game.Player;
 
 /**
  * Created by krzysiek on 6/14/17.
  */
 public class UserOutput {
+    protected char[] board;
 
     public void printWhoStarts(Player player) {
         System.out.println("Player " + player + "will be the first player this round!");
@@ -16,16 +18,26 @@ public class UserOutput {
         System.out.println("Enter your move (row[1-" + rows.toString() +"], column[1-" + cols.toString() + "]: ");
     }
 
-    public void displayBoard() {
-        int[] a = new int[9];
-        a[0] = a[5] = a[6] = 10;
-        a[3] = a[7] = 100;
-        a[1] = a[2] = a[4] = a[8] = 0;
-        for (int i=0; i<9; i+=3) {
-            System.out.println("+---+---+---+");
-            for (int j=i; j<i+3; ++j) {
-                System.out.println("| %s ", a[j] ==10 ? "0" : a[j] ==100 ? "X" : " ");
-            }
+    public static char[] setBoard() {
+        char[] board = new char[9];
+        for (int i = 0; i < board.length; i++) {
+            board[i] = '-';
         }
+        return board;
     }
+
+    public void showBoard() {
+        this.board = setBoard();
+        System.out.println();
+        System.out.println("||TIC TAC TOE||");
+        for (int i = 0; i < board.length; i++) {
+            if (i % 3 == 0 && i != 0) {
+                System.out.println();
+                System.out.println("------------");
+            }
+            System.out.print(" | " + board[i]);
+        }
+        System.out.println();
+    }
+
 }
