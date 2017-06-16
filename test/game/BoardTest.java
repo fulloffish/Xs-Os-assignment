1cde1b9ba137dee3218b32f215abe08c185da850
Krzychu <sk@in.pl>
2017-06-16 06:53:31 +0200

    Correct init test

--- test/game/BoardTest.java
+++ test/game/BoardTest.java
@@ -18,19 +18,12 @@ class BoardTest {
     @Test
     @DisplayName("Init fill cells list with Cells objects")
     public void testIfInitSetsCellsInProperPlaces() {
-
-        assertAll("Every place is filled",
-                () -> assertNotNull(board.getCell(0, 0)),
-                () -> assertNotNull(board.getCell(0, 1)),
-                () -> assertNotNull(board.getCell(0, 2)),
-                () -> assertNotNull(board.getCell(1, 0)),
-                () -> assertNotNull(board.getCell(1, 1)),
-                () -> assertNotNull(board.getCell(1, 2)),
-                () -> assertNotNull(board.getCell(2, 0)),
-                () -> assertNotNull(board.getCell(2, 1)),
-                () -> assertNotNull(board.getCell(2, 2))
-        );
-    }
+        for(int rowIndex = 0; rowIndex < board.ROWS; rowIndex++){
+            for(int columnIndex = 0; columnIndex < board.COLS; columnIndex++) {
+                assertNotNull(board.getCell(rowIndex, columnIndex));
+                }
+            }
+        }
 
     @Test
     @DisplayName("IsDraw() method checks if there is a draw")
